2e96884b277e2cfe698ac72297e972f446d59eb4
hkolsen <swisswebmistress@gmail.com>
2020-06-13 17:43:59 -0700

    ✏️ Add Code of Conduct

--- src/components/CodeOfConduct/CodeOfConduct.tsx
+++ src/components/CodeOfConduct/CodeOfConduct.tsx
@@ -4,22 +4,44 @@ import { useCOCData } from '~/data/useCOCData';
 import { MarkdownWrapper } from '../MarkdownWrapper';
 
 const Wrapper = styled.div`
-    padding: 10em 0 3em;
+    padding: 3em 0;
     position: relative;
+    max-width: 1290px;
+    margin: 0 auto;
     width: 100%;
     ${({ theme }) => theme.media.medium`
-        padding: 7em 0 3em;
+        padding: 1.5em;
+        width: 90%;
     `}
 `;
 
+
+const Header = styled.h2`
+  background: ${({ theme }) => theme.colors.ACCENT};
+  border: 2px solid ${({ theme }) => theme.colors.DARK_GRAY};
+  box-shadow: ${({ theme }) => theme.colors.DARK_GRAY} -8px 8px;
+  color: ${({ theme }) => theme.colors.BASE};
+  display: inline-block;
+  font-family: ${({ theme }) => theme.fonts.SANS_SERIF};
+  font-size: 3em;
+  font-weight: ${({ theme }) => theme.weights.SEMI_BOLD};
+  margin: 0 0 0.5em;
+  padding: 0.25em 0.5em;
+  ${({ theme }) => theme.media.medium`
+      font-size: 2.5em;
+  `};
+`;
+
 const Code = styled(MarkdownWrapper)`
-    color: ${({ theme }) => theme.colors.WHITE};
-    max-width: 29em;
+    color: ${({ theme }) => theme.colors.BASE};
     padding: 0 0 1em;
+    h2 {
+        margin: 0 0 0.5em;
+    }
     p {
-        font-size: 1.25em;
+        font-size: 1em;
         line-height: 1.5;
-        margin: 0;
+        margin: 0 0 0.5em;
         padding: 0;
         span {
             background: ${({ theme }) => theme.colors.ACCENT};
@@ -30,7 +52,7 @@ const Code = styled(MarkdownWrapper)`
     ${({ theme }) => theme.media.medium`
         min-height: 10em;
         padding: 0 0.5em 1em; 
-        width: 100%;
+        width: 90%;
         br {
             display: none;
         }
@@ -41,6 +63,9 @@ export const CodeOfConduct: React.FC = () => {
     const { html } = useCOCData();
     return (
     <Wrapper>
+        <Header>
+        Check Out The W Code of Conduct
+          </Header>
         <Code content={html} />
     </Wrapper>
   );
